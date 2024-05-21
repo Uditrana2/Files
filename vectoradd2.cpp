34557638a88b2ad9bdb74d7feee4b78eee356db1
UDIT <126059588+Uditrana2@users.noreply.github.com>
2024-05-21 21:47:01 +0530

    Update vectoradd2.cpp

--- vectoradd2.cpp
+++ vectoradd2.cpp
@@ -1,19 +1,33 @@
- 
- 	
 #include <bits/stdc++.h>
 #include <omp.h>
+
 using namespace std;
+using namespace std::chrono;
+
 int main() {
-    const int N = 1000000000; 
+    const int N = 10000000; 
     vector<int> vecA(N, 1);
     vector<int> vecB(N, 2);
-    vector<int> vecC(N, 0); 
-    double starttime = omp_get_wtime();
+    vector<int> vecC(N, 0);
+
+    auto start = high_resolution_clock::now();
+    for (int i = 0; i < N; ++i) {
+        vecC[i] = vecA[i] + vecB[i];
+    }
+    auto end = high_resolution_clock::now();
+    duration<double> sequentialDuration = end - start;
+    cout << "Sequential execution time: " << sequentialDuration.count() << " seconds" << endl;
+
+    
+    start = high_resolution_clock::now();
     #pragma omp parallel for
     for (int i = 0; i < N; ++i) {
         vecC[i] = vecA[i] + vecB[i];
     }
-    double endtime = omp_get_wtime();
-    cout<<"done in : "<<endtime-starttime<<"s"<<endl;
+    end = high_resolution_clock::now();
+    duration<double> parallelDuration = end - start;
+    cout << "Parallel execution time: " << parallelDuration.count() << " seconds" << endl;
+
     return 0;
 }
+
